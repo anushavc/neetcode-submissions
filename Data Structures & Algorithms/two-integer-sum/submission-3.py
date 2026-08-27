445aef301af2d59f3d05024cc27d69c7285d7051
Anusha Verma Chandraju <61351780+anushavc@users.noreply.github.com>
2026-08-27 16:59:55 -0500

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #hashmap to see if you have seen that number before key being the number and value being index
+        hashmap={}
+        for i in range(len(nums)):
+            if target-nums[i] in hashmap:
+                return [hashmap[target-nums[i]],i]
+            else:
+                hashmap[nums[i]]=i
+        return []
+
+        
+        
